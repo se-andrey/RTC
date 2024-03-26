d11bbf1e228b66b97756ee6f43a76f28d0e3d9bc
Andrey <andrey-se@yandex.ru>
2024-03-26 11:44:02 +0300

    2 partitions for topic, optimize consumer

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,35 +1,37 @@
 import asyncio
 import logging
 import os
-from aiokafka import AIOKafkaConsumer
+from aiokafka import AIOKafkaConsumer, TopicPartition
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
 
 
-async def consume_messages(consumer_id: str, topic: str, key: str):
+async def consume_messages(consumer_id: str, topic: str, part: int, kafka_server: str):
     """
     функция для обработки сообщений из Kafka
 
+    :param kafka_server: сервер Kafka
     :param consumer_id: id консьюмера
     :param topic: Название темы Kafka, из которой будут получены сообщения.
-    :param key: Ключ, по которому отбираются сообщения для обработки.
+    :param part: Номер партиции, на которую подписывается консьюмер
     :return:
     """
     consumer = AIOKafkaConsumer(
-        topic,
-        bootstrap_servers="kafka:9092",
+        bootstrap_servers=kafka_server,
         group_id=consumer_id,
         enable_auto_commit=True,
         auto_offset_reset="earliest",
-
     )
 
     try:
         await consumer.start()
+
+        topic_partition = TopicPartition(topic, part)           # подписываем консьюмера на топик и партицию
+        consumer.assign([topic_partition])
+
         async for message in consumer:
-            if message.key == key.encode('utf-8'):  # проверяем кто должен обработать сообщение
-                await asyncio.sleep(0.1)            # симулируем обработку сообщения
-                logging.info(f'{consumer_id} received message: {message.value.decode()}')
+            await asyncio.sleep(0.1)                            # симулируем обработку сообщения
+            logging.info(f'{consumer_id} received message: {message.value.decode()}')
 
     except Exception as e:
         logging.error(f"Error: {e}")
@@ -39,10 +41,11 @@ async def consume_messages(consumer_id: str, topic: str, key: str):
 
 
 async def main():
-    consumer_id = os.getenv("CONSUMER_ID")     # id группы из окружения
-    topic = os.getenv("TOPIC")                  # topic из окружения
-    key = os.getenv("KEY")                      # key из окружения
-    await consume_messages(consumer_id, topic, key)
+    kafka_server = os.getenv("KAFKA_BOOTSTRAP_SERVERS")         # сервер кафки
+    consumer_id = os.getenv("CONSUMER_ID")                      # id группы из окружения
+    topic = os.getenv("TOPIC")                                  # topic из окружения
+    part = int(os.getenv("PART"))                               # номер партиции из окружения
+    await consume_messages(consumer_id, topic, part, kafka_server)
 
 
 asyncio.run(main())
--- producer/producer.py
+++ producer/producer.py
@@ -3,32 +3,57 @@ import logging
 import os
 
 from aiokafka import AIOKafkaProducer
-
+from aiokafka.admin import AIOKafkaAdminClient, NewTopic
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
 
 
-async def producer_messages():
+async def create_topic_if_not_exists(topic_name: str, kafka_server: str):
+    """
+    Проверяет и создает топик на с 2я партициями и 1м фактором репликации
+
+    :param topic_name: Название топика
+    :param kafka_server: сервер Kafka
+    :return:
+    """
+    admin_client = AIOKafkaAdminClient(bootstrap_servers=kafka_server)
+    await admin_client.start()
+
+    topics = await admin_client.list_topics()
+
+    if topic_name not in topics:
+        new_topic = NewTopic(topic_name, num_partitions=2, replication_factor=1)
+        result = await admin_client.create_topics([new_topic])
+        logging.info(f"Topic '{topic_name}' make {result}")
+    else:
+        logging.info(f"Topic '{topic_name}' exist")
+
+    await admin_client.close()
+
+
+async def producer_messages(topic_name: str, kafka_server: str):
     """
     Функция для отправки сообщений в Kafka.
+
+    :param topic_name: Название топика
+    :param kafka_server: сервер Kafka
     :return:
     """
 
     producer = AIOKafkaProducer(
-        bootstrap_servers="kafka:9092",
+        bootstrap_servers=kafka_server,
     )
 
     try:
         await producer.start()
 
-        topic = os.getenv("TOPIC")
         messages = ((b"Message 1", b"key1"), (b"Message 2", b"key2"))
         index = 0
 
         while True:
-
+            part = index % 2
             message, key = messages[index % 2]          # делим сообщения между двумя консьюмерами
-            await producer.send_and_wait(topic, message, key=key)
+            await producer.send(topic_name, message, key=key, partition=part)
             logging.info(f"Message sent: {message.decode()}")
             index += 1
             await asyncio.sleep(5)
@@ -40,5 +65,13 @@ async def producer_messages():
         await producer.stop()
 
 
-asyncio.run(producer_messages())
+async def main():
+    topic_name = os.getenv("TOPIC")
+    kafka_server = os.getenv("KAFKA_BOOTSTRAP_SERVERS")
+    await create_topic_if_not_exists(topic_name, kafka_server)
+    await producer_messages(topic_name, kafka_server)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
 
